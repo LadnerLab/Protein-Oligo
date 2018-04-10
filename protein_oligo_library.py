c35704ac209c9065fc5caa1cbe539813fb1915f3
Bob Costas <finkzane@gmail.com>
2018-04-10 13:33:37 -0700

    Added read_fasta_lists method to retrieve a list of names and sequences from a fasta file

--- protein_oligo_library.py
+++ protein_oligo_library.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+
+def main():
+    in_file = open( "lassa_seq_example.fasta" , 'r' )
+    names, sequences = read_fasta_lists( in_file )
+    print( names )
+    print( sequences )
+    
+def read_fasta_lists( file_to_read ):
+    """
+       Reads a list of fastas from file_to_read
+    
+       Returns:
+        names- a list of names of the sequences found in the fasta file
+        sequences- a list of the sequences found in the fasta file
+    """
+
+    names = []
+    sequences = []
+
+    for current_line in file_to_read.readlines():
+
+        current_line = current_line.strip()
+        if current_line[ 0 ] == '>':
+            current_line = current_line.split( '>' )
+            names.append( current_line[ 1 ] )
+
+        else:
+            sequences.append( current_line )
+
+    return names[ 0: ], sequences
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+    
