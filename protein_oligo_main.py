72eb0ae696f012d9acd13cad6753ed9f7f6e458b
Bob Costas <finkzane@gmail.com>
2018-04-11 11:59:06 -0700

    Added windowSize option to option_parser

--- protein_oligo_main.py
+++ protein_oligo_main.py
@@ -10,10 +10,14 @@ def main():
 
 
 def add_program_options( option_parser ):
+    option_parser.add_program_options( '-w', '--windowSize', type = 'int', \
+                                       default = 100, \
+                                       help = "Amount of characters from each alignment sequence to look at."
+                                     )
     option_parser.add_option( '-s', '--stepSize', type = 'int', \
                              default = option_parser.windowSize, \
                              help = "Amount of space to move over after each alignment of sequences. Default is set to window_size."
-                           )
+                            )
 
 
 if __name__ == '__main__':
