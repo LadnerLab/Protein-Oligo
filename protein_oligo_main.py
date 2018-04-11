b1f5ab0e7845d897c612550f8c4b5af1f7786995
Bob Costas <finkzane@gmail.com>
2018-04-11 12:06:03 -0700

    Added option for alignment file and check to make sure that an alignment file is provided to the program

--- protein_oligo_main.py
+++ protein_oligo_main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import protein_oligo_library as oligo
+import sys
 import optparse
 
 def main():
@@ -10,15 +11,23 @@ def main():
 
    options, arguments = option_parser.parse_args()
 
+   # Make sure that a file was provided to script
+   if options.alignment is None:
+       print( "Fasta alignment file must be provided, exiting." )
+       sys.exit( 1 )
+
 
 def add_program_options( option_parser ):
+    option_parser.add_option( '-a', '--alignment', help = "Fasta query file of sequence alignment to be used by program. [None, Required]"
+                            )
+
     option_parser.add_option( '-w', '--windowSize', type = 'int', \
                                        default = 100, \
-                                       help = "Amount of characters from each alignment sequence to look at. Default is 100"
+                                       help = "Amount of characters from each alignment sequence to look at. [100]"
                                      )
     option_parser.add_option( '-s', '--stepSize', type = 'int', \
                              default = 100, \
-                             help = "Amount of space to move over after each alignment of sequences. Default is 100"
+                             help = "Amount of space to move over after each alignment of sequences. [100]"
                             )
 
 
