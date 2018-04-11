1d464a8701ac23d3569e025494fb5209a05e8177
Bob Costas <finkzane@gmail.com>
2018-04-11 11:56:58 -0700

    Created option_parser object, added stepSize to options

--- protein_oligo_main.py
+++ protein_oligo_main.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 import protein_oligo_library as oligo
+import optparse
+
+def main():
+   usage = "usage: %prog [options]"
+   option_parser = optparse.OptionParser( usage )
+
+   add_program_options( option_parser )
+
+
+def add_program_options( option_parser ):
+    option_parser.add_option( '-s', '--stepSize', type = 'int', \
+                             default = option_parser.windowSize, \
+                             help = "Amount of space to move over after each alignment of sequences. Default is set to window_size."
+                           )
+
 
 if __name__ == '__main__':
-    in_file = open( "lassa_seq_example.fasta" , 'r' )
-    names, sequences = oligo.read_fasta_lists( in_file )
-    print( names )
-    print( sequences )
- 
+    main()
+
+
+
+
